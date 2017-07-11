fb5871745f066d5e506c039b76e7f0e62f320c94
knupfer <fknupfer@gmail.com>
2017-07-11 12:39:54 +0200

    simplify coredump

--- src/Main.hs
+++ src/Main.hs
@@ -4,11 +4,10 @@
 module Main where
 
 import Html
-import qualified Data.Text.Lazy.IO as T
-import qualified Data.Text.Lazy.Builder as T
 
 main :: IO ()
-main = T.putStrLn $ T.toLazyText $ render (bigTable (1000,50))
+main
+  = print
+  . div_
+  $ div_ "TEST"
 
-bigTable :: (Int, Int) -> 'Table > ['Tr > ['Td > String]]
-bigTable (n, m) = table_ $ replicate n (tr_ $ map (td_ . show) [1..m])
