be9cefdf2bf7e12c6e6b127a8b8758f59aa88a93
knupfer <fknupfer@gmail.com>
2017-08-04 19:03:25 +0200

    bump version

--- type-of-html.nix
+++ type-of-html.nix
@@ -3,7 +3,7 @@
 }:
 mkDerivation {
   pname = "type-of-html";
-  version = "0.2.0.0";
+  version = "0.2.1.0";
   src = ./.;
   libraryHaskellDepends = [ base text ];
   testHaskellDepends = [ base hspec text ];
