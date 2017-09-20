aa71178a2958aee8de0a25c15a245e7975f2e00f
knupfer <fknupfer@gmail.com>
2017-09-20 10:56:02 +0200

    clean up test suite

--- test/Main.hs
+++ test/Main.hs
@@ -6,201 +6,214 @@
 module Main where
 
 import Html
+
 import qualified Html.Attribute as A
 
 import Data.Proxy
 import Test.Hspec
 import Test.QuickCheck
 
-import Data.Text.Lazy.Encoding
-
-import qualified Data.Text.Lazy as T
-
-newtype Escaped = Escaped String deriving Show
-
-instance Arbitrary Escaped where
-  arbitrary = Escaped <$> arbitrary `suchThat` (\x -> all (`notElem` x) "<>&\"'")
-
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec = let allT a b c = b (renderString a, T.unpack $ renderText a, T.unpack . decodeUtf8 $ renderByteString a)
-                          (c, c, c)
-
-       in parallel $ do
+spec = parallel $ do
 
   describe "render" $ do
 
-    it "is id on strings without escaping chars" $ do
+    it "is id on strings without escaping" $ do
 
-      property $ \(Escaped x) -> allT x (==) x
+      property $ \x ->
+        renderString (Raw x)
+        ===
+        x
 
     it "handles single elements" $ do
 
-      property $ \(Escaped x) -> allT (div_ x) (==) ("<div>" ++ x ++ "</div>")
+      property $ \x ->
+        renderString (div_ (Raw x))
+        ===
+        "<div>" ++ x ++ "</div>"
 
     it "handles nested elements" $ do
 
-      property $ \(Escaped x) -> allT (div_ (div_ x)) (==)
-        ("<div><div>" ++ x ++ "</div></div>")
+      property $ \x ->
+        renderString (div_ (div_ (Raw x)))
+        ===
+        "<div><div>" ++ x ++ "</div></div>"
 
     it "handles parallel elements" $ do
 
-      property $ \(Escaped x) (Escaped y) -> allT (div_ x # div_ y)
-        (==)
-        ("<div>" ++ x ++ "</div><div>" ++ y ++ "</div>")
+      property $ \x y ->
+        renderString (div_ (Raw x) # div_ (Raw y))
+        ===
+        "<div>" ++ x ++ "</div><div>" ++ y ++ "</div>"
 
     it "doesn't use closing tags for empty elements" $ do
 
-      allT area_
-        shouldBe
+      renderString area_
+        `shouldBe`
         "<area>"
 
-      allT base_
-        shouldBe
+      renderString base_
+       `shouldBe`
         "<base>"
 
-      allT br_
-        shouldBe
+      renderString br_
+       `shouldBe`
         "<br>"
 
-      allT col_
-        shouldBe
+      renderString col_
+       `shouldBe`
         "<col>"
 
-      allT embed_
-        shouldBe
+      renderString embed_
+       `shouldBe`
         "<embed>"
 
-      allT hr_
-        shouldBe
+      renderString hr_
+       `shouldBe`
         "<hr>"
 
-      allT iframe_
-        shouldBe
+      renderString iframe_
+       `shouldBe`
         "<iframe>"
 
-      allT img_
-        shouldBe
+      renderString img_
+       `shouldBe`
         "<img>"
 
-      allT link_
-        shouldBe
+      renderString link_
+       `shouldBe`
         "<link>"
 
-      allT menuitem_
-        shouldBe
+      renderString menuitem_
+       `shouldBe`
         "<menuitem>"
 
-      allT meta_
-        shouldBe
+      renderString meta_
+       `shouldBe`
         "<meta>"
 
-      allT param_
-        shouldBe
+      renderString param_
+       `shouldBe`
         "<param>"
 
-      allT source_
-        shouldBe
+      renderString source_
+       `shouldBe`
         "<source>"
 
-      allT track_
-        shouldBe
+      renderString track_
+       `shouldBe`
         "<track>"
 
-      allT wbr_
-        shouldBe
+      renderString wbr_
+       `shouldBe`
         "<wbr>"
 
     it "handles trailing text" $ do
 
-      property $ \(Escaped x) (Escaped y) -> allT (td_ x # y)
-        (==)
-        ("<td>" ++ x ++ "</td>" ++ y)
+      property $ \x y ->
+        renderString (td_ (Raw x) # (Raw y))
+        ===
+        "<td>" ++ x ++ "</td>" ++ y
 
     it "handles a single compile time text" $ do
 
-      allT (Proxy :: Proxy "a")
-        shouldBe
+      renderString (Proxy :: Proxy "a")
+       `shouldBe`
         "a"
 
     it "handles trailing compile time text" $ do
 
-      allT (div_ "a" # (Proxy :: Proxy "b"))
-        shouldBe
+      renderString (div_ "a" # (Proxy :: Proxy "b"))
+       `shouldBe`
         "<div>a</div>b"
 
     it "handles nested compile time text" $ do
 
-      allT (div_ (Proxy :: Proxy "a"))
-        shouldBe
+      renderString (div_ (Proxy :: Proxy "a"))
+       `shouldBe`
         "<div>a</div>"
 
     it "handles an empty list" $ do
 
-      allT (tail [td_ "a"])
-        shouldBe
+      renderString (tail [td_ "a"])
+       `shouldBe`
         ""
 
     it "handles a list with a single element" $ do
 
-      allT [td_ "a"]
-        shouldBe
+      renderString [td_ "a"]
+       `shouldBe`
         "<td>a</td>"
 
     it "handles tags in a list with parallel elements" $ do
 
-      allT [div_ "a" # i_ "b"]
-        shouldBe
+      renderString [div_ "a" # i_ "b"]
+       `shouldBe`
         "<div>a</div><i>b</i>"
 
     it "handles nested lists" $ do
 
-      allT (div_ [div_ [div_ (4 :: Int)]])
-        shouldBe
+      renderString (div_ [div_ [div_ (4 :: Int)]])
+       `shouldBe`
         "<div><div><div>4</div></div></div>"
 
     it "handles utf8 correctly" $ do
 
-      allT (div_ "a ä € 𝄞")
-        shouldBe
+      renderString (div_ "a ä € 𝄞")
+       `shouldBe`
         "<div>a ä € 𝄞</div>"
 
-      allT (img_A (A.id_ "a ä € 𝄞"))
-        shouldBe
+      renderString (img_A (A.id_ "a ä € 𝄞"))
+       `shouldBe`
         "<img id=\"a ä € 𝄞\">"
+
+    it "handles Chars" $ do
+
+      property $ \x ->
+        renderString (div_ [x :: Char])
+        ===
+        renderString (div_ x)
+
+    it "handles Ints" $ do
+
+      property $ \x ->
+        renderString (div_ (x :: Int))
+        ===
+        renderString (div_ (show x))
 
     it "handles complex compile time documents" $ do
 
-      allT (div_ () # i_ ())
-        shouldBe
+      renderString (div_ () # i_ ())
+       `shouldBe`
         "<div></div><i></i>"
 
-      allT (div_ () # "a")
-        shouldBe
+      renderString (div_ () # "a")
+       `shouldBe`
         "<div></div>a"
 
-      allT ("a" # i_ ())
-        shouldBe
+      renderString ("a" # i_ ())
+       `shouldBe`
         "a<i></i>"
 
-      allT (div_ () # i_ (Proxy @"a"))
-        shouldBe
+      renderString (div_ () # i_ (Proxy @"a"))
+       `shouldBe`
         "<div></div><i>a</i>"
 
-      allT (div_ (Proxy @"a") # i_ ())
-        shouldBe
+      renderString (div_ (Proxy @"a") # i_ ())
+       `shouldBe`
         "<div>a</div><i></i>"
 
-      allT (Proxy @"1" # "2")
-        shouldBe
+      renderString (Proxy @"1" # "2")
+       `shouldBe`
         "12"
 
-      allT ("1" # Proxy @"2")
-        shouldBe
+      renderString ("1" # Proxy @"2")
+       `shouldBe`
         "12"
 
-      allT (div_ () # td_ (Proxy @"1" # "2" # div_ () # i_A (A.id_ (Proxy @"3")) "4"))
-        shouldBe
+      renderString (div_ () # td_ (Proxy @"1" # "2" # div_ () # i_A (A.id_ (Proxy @"3")) "4"))
+       `shouldBe`
         "<div></div><td>12<div></div><i id=\"3\">4</i></td>"
