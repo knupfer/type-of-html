06a8306afe1cd0ab7263e68d722eab697d92077b
knupfer <fknupfer@gmail.com>
2017-10-29 08:22:20 +0100

    bump version

--- type-of-html.nix
+++ type-of-html.nix
@@ -3,7 +3,7 @@
 }:
 mkDerivation {
   pname = "type-of-html";
-  version = "1.0.0.0";
+  version = "1.0.0.1";
   src = ./.;
   libraryHaskellDepends = [
     base bytestring double-conversion ghc-prim text
