08e08eb3b30214f88c349dcb3c992e7413a8fd63
knupfer <fknupfer@gmail.com>
2017-12-29 00:03:26 +0100

    use a cache for travis

--- release.nix
+++ release.nix
@@ -3,7 +3,7 @@
 with nixpkgs.haskell.lib;
 let
   eval = ghc: import ./default.nix { compiler = ghc; };
-  doBench = drv: overrideCabal drv (drv: { isExecutable = true; buildTarget = "test bench"; configureFlags = "--enable-benchmarks --enable-tests"; });
+  doBench = drv: overrideCabal drv (drv: { isExecutable = true; buildTarget = "test bench"; configureFlags = "--enable-benchmarks --enable-tests --disable-optimization"; });
 in rec {
      ghc802 = buildStrictly (eval "ghc802");
      ghc822 = buildStrictly (eval "ghc822");
