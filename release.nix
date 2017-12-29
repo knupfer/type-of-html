723ccfc6258191e9eaac034b92dd67dc2a8f44ab
knupfer <fknupfer@gmail.com>
2017-12-29 11:01:26 +0100

    appease travis

--- release.nix
+++ release.nix
@@ -5,10 +5,8 @@ let
   eval = ghc: import ./default.nix { compiler = ghc; };
   doBench = drv: overrideCabal drv (drv: { isExecutable = true; buildTarget = "test bench"; configureFlags = "--enable-benchmarks --enable-tests --disable-optimization"; });
 in rec {
+     sdist = sdistTarball ghc822;
+     bench = doBench (doBenchmark (eval "ghc822"));
      ghc802 = buildStrictly (eval "ghc802");
      ghc822 = buildStrictly (eval "ghc822");
-     #ghcHEAD = eval "ghcHEAD";
-
-     bench = doBench (doBenchmark (eval "ghc822"));
-     sdist = sdistTarball ghc822;
 }
