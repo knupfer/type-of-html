f1ddd947111c0620133410784c3cecb38f2e1f67
knupfer <fknupfer@gmail.com>
2018-03-25 20:41:53 +0200

    appease travis

--- release.nix
+++ release.nix
@@ -4,7 +4,5 @@ with nixpkgs.lib;
 let tested = ["ghc802" "ghc822" "ghc841" ];
     eval = x: import ./default.nix { compiler = x; };
 in
-{ sdist = sdistTarball (buildStrictly (eval (last tested)));
-  bench = (x: overrideCabal x (x: { isExecutable = true; buildTarget = "type value perf alloc"; configureFlags = "--enable-benchmarks --enable-tests --disable-optimization"; }))
-          (doBenchmark (eval (last tested)));
+{ sdist = sdistTarball (eval (last tested));
 } // genAttrs tested (x: buildStrictly (eval x))
