d1ef4fc533055fdeb1f136d4b9a9c7c8a1d3eb11
knupfer <fknupfer@gmail.com>
2018-03-25 15:55:10 +0200

    add type test

---
+++ test/Type.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE CPP #-}
+
+module Main where
+
+#if __GLASGOW_HASKELL__ >= 802
+import GHC.TypeLits
+import Html.Type.Internal
+import Data.Proxy
+
+main :: IO ()
+main = pure ()
+
+  where _x_ = undefined :: Test
+
+type Test =
+  ( ToList ()
+    == Empty
+  , ToList Int
+    == Split
+  , ToList (Proxy "a")
+    == 'FingerTree '[] "a"
+  , ToList ('A > Char)
+    == 'FingerTree '["<a>"] "</a>"
+  , ToList ('A > Char # 'Div > Int)
+    == 'FingerTree '["<a>", "</a><div>"] "</div>"
+  , ToList (('Div :@: ('ClassA := Int)) Int)
+    == 'FingerTree '["<div class=\"","\">"] "</div>"
+  , ToList (('Div :@: ('ClassA := ())) Int)
+    == 'FingerTree '["<div class=\"\">"] "</div>"
+  , ToList (('Div :@: ('ClassA := ())) ())
+    == 'FingerTree '[] "<div class=\"\"></div>"
+  , ToList (('Div :@: ('ClassA := () # 'IdA := ())) ())
+    == 'FingerTree '[] "<div class=\"\" id=\"\"></div>"
+  , ToList (('Div :@: ('ClassA := () # 'IdA := Proxy "ab")) ())
+    == 'FingerTree '[] "<div class=\"\" id=\"ab\"></div>"
+  )
+
+type family a == b where
+  a == a = ()
+  a == b = TypeError ('Text "Unequal types:" ':$$: 'ShowType a ':$$: 'ShowType b)
+
+#else
+main :: IO ()
+main = pure ()
+#endif
