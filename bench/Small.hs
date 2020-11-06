dcd749d2ddb3752af73bf9a5c940c37ae59d1b02
knupfer <fknupfer@gmail.com>
2020-11-06 19:50:50 +0100

    fix warning

--- bench/Small.hs
+++ bench/Small.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE TypeOperators    #-}
 {-# LANGUAGE DataKinds        #-}
@@ -6,23 +7,11 @@ module Small where
 
 import Html
 
--- oneElement
---   :: ('Div ?> a)
---   => a -> 'Div > a
 oneElement = (Div :>)
 
--- nestedElement
---   :: ('Span ?> a)
---   => a -> 'Div > ('Span > a)
 nestedElement x = Div :> Span :> x
 
--- parallelElement
---   :: ('Span ?> a, 'Div ?> a)
---   => a -> ('Div > a) # ('Span > a)
 parallelElement x = Div :> x # Span :> x
 
--- listElement
---   :: ('Div ?> a)
---   => a -> ['Div > a]
 listElement x = [Div :> x]
 
