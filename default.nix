df3705ccc0bfde1f3a96a94c6607cd89271f1c29
knupfer <fknupfer@gmail.com>
2021-02-06 16:40:45 +0100

    update pkgs

--- default.nix
+++ default.nix
@@ -1,16 +1,17 @@
 import ((import <nixpkgs> {}).fetchFromGitHub {
   owner  = "knupfer";
   repo   = "hs-nix-default";
-  rev    = "8970a5e1125985dbac7a5b57b4949a105e7ea759";
-  sha256 = "14klz15dwwyx2141q66rix2m40slis1vmhy0ijn571xjwr8xd9jp";})
+  rev    = "b0a6d3e534eca0ec37fc153fc53db5680e716c26";
+  sha256 = "13spcgj40b9w2gyis5jkbi05vlb5v20vh09h8v5y262j8cw2iykn";})
 
   { project = "type-of-html";
     directory = ./.;
     versions =
       [
-        "8.10.2"
+#        { "9.0.1" = "master";}
+        "8.10.3"
         "8.8.4"
-        { "8.6.5" = "nixpkgs_2019";}
+        { "8.6.5" = "nixpkgs_20_03";}
         { "8.4.4" = "nixpkgs_2018";}
         { "8.2.2" = "nixpkgs_2018";}
       ];
